8f4b7ff2cd4ff2fd72a682d9c215d2edf243f42c
Adam Jermyn <adamjermyn@gmail.com>
2020-07-30 09:58:09 -0400

    Added plots and plot scripts

--- Code/bildsten_fig_1.py
+++ Code/bildsten_fig_1.py
@@ -4,16 +4,15 @@ from loader import load_history_name
 
 
 age_index = 2
-dt_index = 3
 log_g_index = 40
 log_L_index = 38
 log_Teff_index = 37
 
-pms_cutoff = 1e8 # Ignore times before 100 MYr
-agb_cutoff = 6.4e9
+pms_cutoff = 1e8 # Ignore times before 100 MYr (pre-main-sequence)
+agb_cutoff = 1e12 # In case the model ran too long
 
 # Get the 1.2 M_sun Z=0.02 model track
-hist = load_history_name('../ModelTracks/m=1.2_z=0.02.data')
+hist = load_history_name('../ModelTracks/m=1.0_z=0.02.data')
 
 # Extract log_g and log_Teff, slice by age
 age = hist[:,age_index]
--- Code/plotter.py
+++ Code/plotter.py
@@ -4,28 +4,47 @@ from loader import load_history_name
 
 
 age_index = 2
-dt_index = 3
+log_dt_index = 3
 log_g_index = 40
 log_L_index = 38
 log_Teff_index = 37
 
-pms_cutoff = 1e8 # Ignore times before 100 MYr
-agb_cutoff = 6.4e9
+
+pms_cutoff = 1e8 # Ignore times before 100 MYr (pre-main sequence)
+RGB_Teff_cut = 4750
 
 # Get the 1.2 M_sun Z=0.02 model track
-hist = load_history_name('../ModelTracks/m=1.2_z=0.02.data')
+hist = load_history_name('../ModelTracks/m=1.0_z=0.02.data')
 
-# Extract log_g and log_Teff, slice by age
+# Cut off the pre-main sequence
 age = hist[:,age_index]
-sel = (age > pms_cutoff) & (age < agb_cutoff)
-log_g = hist[sel, log_g_index]
-log_Teff = hist[sel, log_Teff_index]
-log_L = hist[sel, log_L_index]
+sel = (age > pms_cutoff)
+
+hist = hist[sel]
 age = age[sel]
 
-fig, ax = plt.subplots()
 
-ax.plot(10**log_Teff, log_L)
-ax.set_xlim([5000,3000])
-ax.set_ylim([1,3.5])
-plt.show()
+# Make a quick logTeff cut
+log_Teff = hist[:,log_Teff_index]
+sel = (10**log_Teff < RGB_Teff_cut)
+
+log_g = hist[sel, log_g_index]
+dt = 10**hist[sel, log_dt_index] # years
+
+age = np.cumsum(dt) # Starting from when T first fell below RGB_Teff_cut
+
+print(age.shape)
+print(log_g.shape)
+
+fig, ax = plt.subplots()
+ax.plot(age, log_g)
+plt.xlabel('Time Since Start of RGB (years)')
+plt.ylabel('$\log g / \mathrm{cm\,s^{-2}}$')
+plt.savefig('../Plots/log_g_1.0_msun.pdf')
+
+fig, ax = plt.subplots()
+ax.plot(log_g, dt)
+plt.xlabel('Time Since Start of RGB (years)')
+plt.ylabel('$\log g / \mathrm{cm\,s^{-2}}$')
+plt.savefig('../Plots/log_g_dt_1.0_msun.pdf')
+
