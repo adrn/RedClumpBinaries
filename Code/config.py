75ad3cc3a031dfadb9c777f9f63614553a766d1c
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-02-04 11:30:13 -0500

    new notebook to get q

---
+++ Code/config.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+nbins = 4
+mh_bins = np.linspace(-1, 0.6, nbins + 1)
--- Code/helpers.py
+++ Code/helpers.py
@@ -2,6 +2,7 @@ import pathlib
 from urllib.request import urlopen
 import requests
 from tqdm import tqdm
+import astropy.table as at
 
 
 def download_file(url, local_path=None, local_filename=None, block_size=102400):
@@ -23,3 +24,35 @@ def download_file(url, local_path=None, local_filename=None, block_size=102400):
 
     progress_bar.close()
     return full_local_path
+
+
+local_cache_path = pathlib.Path('../data/').resolve().absolute()
+local_cache_path.mkdir(exist_ok=True)
+
+
+def get_allstar():
+    allstar_path = local_cache_path / 'allStarLite-dr17-synspec_rev1.fits'
+    if not allstar_path.exists():
+        url = "https://data.sdss.org/sas/dr17/apogee/spectro/aspcap/dr17/synspec_rev1/allStarLite-dr17-synspec_rev1.fits"
+        download_file(
+            url, 
+            local_path=allstar_path.parent, 
+            local_filename=allstar_path.parts[-1]
+        )
+    allstar = at.QTable.read(allstar_path)
+    return allstar
+
+
+def get_metadata():
+    metadata_path = local_cache_path / 'metadata.fits'
+    if not metadata_path.exists():
+        url = "https://users.flatironinstitute.org/~apricewhelan/data/apogee-dr17-binaries/metadata.fits"
+        download_file(
+            url, 
+            local_path=metadata_path.parent, 
+            local_filename=metadata_path.parts[-1]
+        )
+    metadata = at.QTable.read(metadata_path)
+    return metadata
+
+
