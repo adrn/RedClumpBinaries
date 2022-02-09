a0d3458f27ad19e1ca114df517d2ff292c1b89a1
Adam Jermyn <adamjermyn@gmail.com>
2022-02-09 16:26:04 -0500

    Tweaked bins

--- Production Code/config.py
+++ Production Code/config.py
@@ -12,5 +12,5 @@ mh_bins = np.linspace(*metallicity_lims, n_metal_bins + 1)
 q_min = 0.1 # Defines the edge of the CHeB.
 			# Smaller values include more of the CHeB but increase sensitivity to fits/noise.
 
-mass_bins = np.array([0.8,1,1.25,1.5,1.8,2]) # We chop the upper mass range because we don't want the secondary clump.
+mass_bins = np.array([0.8,1,1.25,1.5,1.75,2]) # We chop the upper mass range because we don't want the secondary clump.
 mass_lims = (mass_bins.min(), mass_bins.max())
